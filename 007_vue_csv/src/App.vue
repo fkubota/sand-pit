55c99f080cd57abd3657f063f3a8709392a7db6e
fkubota <rkkubotay@gmail.com>
2020-04-05 18:45:40 +0900

    edit 007_vue_csv, csvを読み込めるようにした:w

--- 007_vue_csv/src/App.vue
+++ 007_vue_csv/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>CSVインポート</h1>
-    <input @change="fileChange" type="file" id="file_input_expense" name="file_input_expense">
+    <v-content>
+      <v-file-input @change='fileChange' label='csv input' accept='.csv' show-size></v-file-input>
+      <v-btn>{{btn_name}}</v-btn>
+    </v-content>
+    <!-- <input @change="fileChange" type="file" id="file_input_expense" name="file_input_expense"> -->
     <v-data-table :headers="headers" :items="workers" class="elevation-1">
       <template slot="items" slot-scope="props">
         <td class="text-xs-right">{{ props.item.code }}</td>
@@ -16,6 +20,7 @@
 export default {
   data: function() {
     return {
+      btn_name: 'first',
       headers: [
         {
           text: "Code",
@@ -24,26 +29,28 @@ export default {
           value: "code"
         },
         { text: "Name", align: "left", value: "name" },
-        { text: "WorkerType", align: "left", value: "workerType" }
+        { text: "Age", align: "left", value: "age" }
       ],
       workers: []
     };
   },
   methods: {
     fileChange: function(e) {
-      const file = e.target.files[0];
+      // const file = e.target.files[0];
+      const file = e
       const reader = new FileReader();
       const workers = [];
 
       const loadFunc = () => {
         const lines = reader.result.split("\n");
+        delete lines[0]
         lines.forEach(element => {
           const workerData = element.split(",");
           if (workerData.length != 3) return;
           const worker = {
             code: workerData[0],
             name: workerData[1],
-            workerType: workerData[2]
+            age: workerData[2]
           };
           workers.push(worker);
         });
