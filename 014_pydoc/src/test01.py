4c7475cca42e93ba5bd21e396b12060fb8ebcb0a
fkubota <rkkubotay@gmail.com>
2020-05-28 19:26:55 +0900

    docstringのテスト

---
+++ 014_pydoc/src/test01.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+class calc:
+    '''
+    計算を行うクラスです
+    '''
+    def sum_(a, b):
+        '''
+        足し算を行う
+
+        Parameters
+        ----------
+        a: float
+        b: float
+
+        Returns
+        -------
+        sum_: float
+        '''
+
+        sum_ = a + b
+        return sum_
+
+    def sub_(a, b):
+        '''
+        引き算を行う
+
+        Parameters
+        ----------
+        a: float
+        b: float
+
+        Returns
+        -------
+        sub_: float
+        '''
+
+        sub_ = a - b
+
+        return sub_
---
+++ 014_pydoc/src/test02.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+class calc2:
+    '''
+    計算を行うクラスです
+    '''
+    def mul_(a, b):
+        '''
+        掛け算を行う
+
+        Parameters
+        ----------
+        a: float
+        b: float
+
+        Returns
+        -------
+        mul_: float
+        '''
+
+        mul_ = a * b
+        return mul_
+
+    def div_(a, b):
+        '''
+        割り算を行う
+
+        Parameters
+        ----------
+        a: float
+        b: float
+
+        Returns
+        -------
+        div_: float
+        '''
+
+        div_ = a / b
+        return div_
