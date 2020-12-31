7711ef4f07ef0e28cbd27cf5b81833a87895cdad
fkubota <rkkubotay@gmail.com>
2020-12-31 22:27:25 +0900

    [update] 025

---
+++ 024_wandb_first/01_sample.py
@@ -0,0 +1,17 @@
+import wandb
+import numpy as np
+
+wandb.init(
+        project='sandpit_024_01',
+        name='01_sample.py',
+        )
+print('wandb start')
+
+wandb.config.dropout = 0.2
+wandb.config.hidden_layer_size = 128
+
+for epoch in range(10):
+    loss = np.exp(-epoch)  # change as appropriate :)
+    wandb.log({'epoch': epoch, 'loss': loss})
+
+print('wnadb end')
